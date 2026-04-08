e21e4378b52ae25fff3bfbf248d93fa68f2f6908
Jaxon-Ford <148885484+Jaxon-Ford@users.noreply.github.com>
2026-04-08 13:48:48 -0500

    added main pages and nav bar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">Jaxon Ford</div>
+
+      {/* Hamburger icon */}
+      <div className="hamburger" onClick={toggleMenu}>
+        <div className={`bar ${isOpen ? "open" : ""}`}></div>
+        <div className={`bar ${isOpen ? "open" : ""}`}></div>
+        <div className={`bar ${isOpen ? "open" : ""}`}></div>
+      </div>
+
+      {/* Navigation links */}
+      <ul className={`nav-links ${isOpen ? "open" : ""}`}>
+        <li>
+          <NavLink to="/" end onClick={() => setIsOpen(false)}>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" onClick={() => setIsOpen(false)}>
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/projects" onClick={() => setIsOpen(false)}>
+            Projects
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/skills" onClick={() => setIsOpen(false)}>
+            Skills
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
