6c6127ddff5b433149ecf06590a8b0eaa1f7c43e
Jaxon-Ford <148885484+Jaxon-Ford@users.noreply.github.com>
2026-04-08 13:58:45 -0500

    modified eslint.config file

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,36 @@
 import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+globalIgnores(["dist"]);
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
-
-  pluginReact.configs.flat["jsx-runtime"],
-
   {
-    rules: {
-      "react/react-in-jsx-scope": "off", // just in case
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+      },
     },
   },
 
+  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat["jsx-runtime"],
+
+  {
+    files: ["**/*.{js,jsx}"],
+    extends: [reactHooks.configs.flat.recommended, reactRefresh.configs.vite],
+    rules: {
+      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+      "react/react-in-jsx-scope": "off",
+    },
+  },
 ]);
-
-
